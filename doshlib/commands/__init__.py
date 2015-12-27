d9c49c802e332c124b4eb207e6d9d888a69656cc
Malcolm VanOrder <mvanorder1390@gmail.com>
2015-12-27 02:01:59 -0500

    Added dropletsnapshots
    Signed-off-by: Malcolm VanOrder <mvanorder1390@gmail.com>

--- doshlib/commands/__init__.py
+++ doshlib/commands/__init__.py
@@ -1,19 +1,23 @@
 import doshlib
 import pprint
+import dateutil.parser as dp
 
 def help(command):
     print "listdroplets"
     print "dropletup <droplet ID>"
     print "dropletdown <droplet ID>"
+    print "dropletsnapshots <droplet ID>"
 
 def invalid(command):
     print "Invalid command: " + command
 
 def listdroplets(args):
-    print('Droplets: ')
     result = doshlib.client.droplets.all()
+    row = ['ID','Name','Status']
+    print("{: >10} {: >40} {: >10}".format(*row))
     for i in result['droplets']:
-        print i['id'],i['name'],i['status']
+        row = [i['id'],i['name'],i['status']]
+        print("{: >10} {: >40} {: >10}".format(*row))
 
 def rebootdroplet(command):
     print('not implimented yet')
@@ -41,3 +45,17 @@ def dropletdown(command):
             print result
     else:
         print "Invalid droplet ID:",droplet_id
+
+def dropletsnapshots(command):
+    droplet_id = int(command.split()[1])
+    if doshlib.isdroplet(int(droplet_id)):
+        snapshots = doshlib.client.droplets.get_droplet_snapshots(droplet_id)['snapshots']
+        row = ['Created at','Disto','ID','Min disk size','Name','Public','Regions']
+        print("{: >20} {: >10} {: >10} {: >15} {: >20} {: >10} {: >10}".format(*row))
+        for i in snapshots:
+            row = [str(dp.parse(i['created_at'])),i['distribution'],i['id'],i['min_disk_size'],i['name'],i['public'],i['regions']]
+            print("{: >20} {: >10} {: >10} {: >15} {: >20} {: >10} {: >10}".format(*row))
+
+    else:
+        print "Invalid droplet ID:",droplet_id
+
